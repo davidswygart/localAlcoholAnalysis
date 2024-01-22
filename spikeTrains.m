0a84be6528b99d035df41d1a6f22dfab038002fe
davidswygart <davidswygart@gmail.com>
2024-01-22 12:36:30 -0500

    plot subrefractory spikes

--- spikeTrains.m
+++ spikeTrains.m
@@ -1,66 +1,93 @@
-dataDir = 'C:\Users\dis006\OneDrive - Indiana University\localAlcohol\ephysData\';
 binSize = 0.1; %bin width in seconds
-maxTime = 55*60; %max time in seconds
+maxTime = 50*60; %max time in seconds
 xA = 0:binSize:maxTime;
+nbins = length(xA)-1;
 
-matFiles = ls([dataDir, '*.mat']);
+matFiles = ls('*.mat');
 nFiles = size(matFiles,1);
 
 for i=1:nFiles
-    load([dataDir, matFiles(i,:)])
+    %concatonate all spikes
+    load(matFiles(i,:))
 
     spk = [spkGood; spkMUA; spkNoise];
-    
-
     nGood = length(spkGood);
     nMUA = length(spkMUA);
     nNoise = length(spkNoise);
-
     clusterIdentity = zeros([nMUA+nGood+nNoise, 1]);
     clusterIdentity(1:nGood) = 2;
     clusterIdentity(nGood+1: nGood+nMUA) = 1;
     
+    %Get rid of cluster with mean ISI > 10s
+    haveEnoughSpikes = cellfun(@(x) mean(diff(x)), spk)<10;
+    spk = spk(haveEnoughSpikes);
+    clusterIdentity = clusterIdentity(haveEnoughSpikes);
     nclusters = length(spk);
-    nbins = length(xA)-1;
+    
+    %Examine clusters with unusually low ISI
+    figure(1)
+    cID = 1;
+    exampleSpk = spk{cID};
+    plot(exampleSpk(2:end),log10(diff(exampleSpk)))
+    xlabel('time (s)')
+    ylabel('log10(ISI) (s)') 
+    
+    
     spkCounts = nan(nclusters,nbins);
-    spkSmoothed = nan(nclusters,nbins);
+    subRefractoryCounts = nan(nclusters,nbins);
 
     for u =1:length(spk)
         spkCounts(u,:) = histcounts(spk{u}, xA);
-        meanISI = mean(diff(spk{u}));
-
-        if meanISI < 11
-            spkSmoothed(u,:) = imgaussfilt(spkCounts(u,:),meanISI/binSize);
-        else
-            spkSmoothed(u,:) = nan;
-        end
+        
+        subRefractoryThreshold = 0.001;
+        isSubRefractory = diff(spk{u}) < subRefractoryThreshold;
+        subSpikes = spk{u};
+        subSpikes  = subSpikes(2:end);
+        subSpikes = subSpikes(isSubRefractory);
+        subRefractoryCounts(u,:) = histcounts(subSpikes, xA);
+        
+%         meanISI = mean(diff(spk{u}));
+%         if meanISI < 11
+%             spkSmoothed(u,:) = imgaussfilt(spkCounts(u,:),meanISI/binSize/4);
+%         else
+%             spkSmoothed(u,:) = nan;
+%         end
     end
 
+    
+    
 
-    spkSmoothedNorm = spkSmoothed ./ max(spkSmoothed, [],2);
-    spkZscore = zscore(spkSmoothed, 0, 2);
 
+%     spkNorm = spkCounts ./ max(spkCounts, [],2);
+%     figure(1)
+%     imagesc(spkNorm)
+%     colorbar
+%     title("spike normalized")
+%     hold on
+%     plot([0, size(spkCounts,2)],[nGood+nMUA+1,nGood+nMUA+1], 'r')
+%     hold off
 
-    figure(1)
-    imagesc(spkCounts)
-    colorbar
-    title("spike counts")
+    spkZscore = zscore(spkCounts, 0, 2);
 
     figure(2)
-    imagesc(spkSmoothed)
+    subplot(2,1,1)
+    imagesc(spkZscore, [-2 2])
     colorbar
-    title("spikes smoothed")
-
-    figure(3)
-    imagesc(spkSmoothedNorm)
+    title("spike Zscore")
+    hold on
+    firstNoiseInd = find(clusterIdentity==0, 1);
+    plot([0, size(spkCounts,2)],[firstNoiseInd,firstNoiseInd], 'r')
+    hold off
+    
+    subplot(2,1,2)
+    subRefractoryCounts(subRefractoryCounts==0) = nan;
+    imagesc(subRefractoryCounts, 'alphadata', 0+~isnan(subRefractoryCounts))
     colorbar
-    title("spike smoothed normalized")
-
-
-    figure(4)
-    imagesc(spkZscore)
-    colorbar
-    title("spike smoothed Zscore")
+    title('Subrefractory spikes')
+    hold on
+    plot([0, size(spkCounts,2)],[firstNoiseInd,firstNoiseInd], 'r')
+    hold off
+    
 
 % waveforms
 %     ind = 1;
