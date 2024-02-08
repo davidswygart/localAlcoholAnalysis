dd19935a187ac0203ecddfea7c11a17af6437843
davidswygart <davidswygart@gmail.com>
2024-02-08 12:23:50 -0500

    Update addQualityMetics.m
    save new table with quality metric values

--- addQualityMetics.m
+++ addQualityMetics.m
@@ -2,8 +2,12 @@
 matFiles = ls('*.mat');
 nFiles = size(matFiles,1);
 
+saveFolder = 'withQualityMetrics';
+mkdir(saveFolder)
+
 for i=1:nFiles
     load(matFiles(i,:))
     clusterInfo = calcIsiViolations(clusterInfo);
     clusterInfo = calcPresenceRatio(clusterInfo);
+    save([saveFolder,filesep,matFiles(i,:)], "channel","clusterInfo","events","paramsFile","waveformsTime")
 end
