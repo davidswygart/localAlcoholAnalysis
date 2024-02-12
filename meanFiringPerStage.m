f51b5334a25e4bfe022838c61c55f1a27493d0bd
David Swygart <davidswygart2015@u.northwestern.edu>
2024-02-12 18:04:08 -0500

    Update meanFiringPerStage.m

--- meanFiringPerStage.m
+++ meanFiringPerStage.m
@@ -41,4 +41,64 @@ control = allClusters(ismember(allClusters.matName, group.matName(contains(group
 drink = allClusters(ismember(allClusters.matName, group.matName(contains(group.group, 'drink'))),:);
 inject = allClusters(ismember(allClusters.matName, group.matName(contains(group.group, 'injected'))),:);
 
-%control.avgSpk
+avgControl = mean(cell2mat(control.avgSpk),1);
+stdControl = std(cell2mat(control.avgSpk), [], 1);
+nControl = size(control,1);
+
+avgDrink= mean(cell2mat(drink.avgSpk),1);
+stdDrink = std(cell2mat(drink.avgSpk), [], 1);
+nDrink = size(drink,1);
+
+avgInject = mean(cell2mat(inject.avgSpk),1);
+stdInject = std(cell2mat(inject.avgSpk), [], 1);
+nInject = size(inject,1);
+
+
+names = ["baseline","inject","post-inj","sipper","post-sip"];
+figure(1)
+clf
+X = categorical({'baseline','inject','post-inj','sipper','post-sip'});
+X = reordercats(X,{'baseline','inject','post-inj','sipper','post-sip'});
+bar(X,[avgControl',avgDrink',avgInject'])
+ylabel('mean spike rate')
+legend('Control','Drink alcohol', 'Inject alcohol')
+
+
+
+figure(2)
+clf
+subplot(3,1,1)
+violinplot(cell2mat(control.avgSpk),names)
+ylim([0,15])
+ylabel('Control (spike rate)')
+subplot(3,1,2)
+violinplot(cell2mat(drink.avgSpk), names)
+ylim([0,15])
+ylabel('Drink alcohol (spike rate)')
+subplot(3,1,3)
+violinplot(cell2mat(inject.avgSpk), names)
+ylim([0,15])
+ylabel('Inject alcohol (spike rate)')
+
+
+figure(3)
+clf
+subplot(3,1,1)
+y = cell2mat(control.avgSpk);
+y = y(:, 2:end) ./ y(:, 1);
+y = log(y);
+violinplot(y,names(2:end))
+ylabel('Control: log(norm. spike rate)')
+subplot(3,1,2)
+y = cell2mat(drink.avgSpk);
+y = y(:, 2:end) ./ y(:, 1);
+y = log(y);
+y(y<-5000) = -4 ;
+violinplot(y,names(2:end))
+ylabel('Drink alcohol: log(norm. spike rate)')
+subplot(3,1,3)
+y = cell2mat(inject.avgSpk);
+y = y(:, 2:end) ./ y(:, 1);
+y = log(y);
+violinplot(y,names(2:end))
+ylabel('Inject alcohol: log(norm. spike rate)')
