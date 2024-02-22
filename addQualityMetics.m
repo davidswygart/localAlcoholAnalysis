6794a6240d81830b30f409994fd03f74225e0429
davidswygart <davidswygart@gmail.com>
2024-02-22 11:47:29 -0500

    Update addQualityMetics.m
    add sorting before quality metrics

--- addQualityMetics.m
+++ addQualityMetics.m
@@ -7,6 +7,8 @@ mkdir(saveFolder)
 
 for i=1:nFiles
     load(matFiles(i,:))
+
+    clusterInfo.spikeTimes = cellfun(@sort, clusterInfo.spikeTimes, 'UniformOutput', false);% sort spike times for each cluster
     clusterInfo = calcIsiViolations(clusterInfo);
     clusterInfo = calcPresenceRatio(clusterInfo);
     save([saveFolder,filesep,matFiles(i,:)], "channel","clusterInfo","events","paramsFile","waveformsTime")
