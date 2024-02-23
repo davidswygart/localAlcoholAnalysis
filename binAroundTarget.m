b31d41ddc51a0ee8eda8f3a7095876ae9bb3ffc7
davidswygart <davidswygart@gmail.com>
2024-02-23 13:29:49 -0500

    plot mean centered around different trigger points

---
+++ binAroundTarget.m
@@ -0,0 +1,45 @@
+function [spkCounts,  bpod] = binAroundTarget(c, target, binEdges)
+
+spkCounts = nan(size(c,1), length(binEdges)-1);
+
+for i=1:size(c,1)
+    bpod = c.bpod(i);
+    tVal = bpod.(target);
+
+    %subtract off target value. There should be a better way of doing this.
+    bpod.baselineStart = bpod.baselineStart - tVal;
+    bpod.microInjectionStart = bpod.microInjectionStart - tVal;
+    bpod.postInjectionStart = bpod.postInjectionStart - tVal;
+    bpod.sipperStart = bpod.sipperStart - tVal;
+    bpod.tailStart = bpod.tailStart - tVal;
+    bpod.dropDeployed = bpod.dropDeployed - tVal;
+
+    spk = c.spikeTimes{i};
+    spk = spk-tVal;
+
+    spkCounts(i,:) = histcounts(spk,binEdges);
+end
+
+
+imagesc(spkCounts)
+% hold on
+% binWidth = binEdges(2) - binEdges(1);
+% y=[-5,0];
+% x = bpod.microInjectionStart/binWidth;
+% plot([x,x], y,'r')
+% text(x,y(1), 'inj')
+% x = bpod.sipperStart/binWidth;
+% plot([x,x], y, 'r')
+% text(x,y(1), 'sip')
+% x = bpod.tailStart/binWidth;
+% plot([x,x], y, 'r')
+% text(x,y(1), 'tail')
+% x = bpod.postInjectionStart/binWidth;
+% plot([x,x], y, 'r')
+% text(x,y(1), 'post-inj')
+% 
+% x = bpod.dropDeployed/binWidth;
+% y = ones(length(x),1) * -5 ;
+% scatter(x,y, 'r*')
+% hold off
+end
