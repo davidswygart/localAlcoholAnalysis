261be99e609e47071daed15806cc7d4e5b29bef6
davidswygart <davidswygart@gmail.com>
2024-03-07 17:38:31 -0500

    handles empty x

--- addShadedLine.m
+++ addShadedLine.m
@@ -4,6 +4,9 @@ function h = addShadedLine(x, ymat, color, name)
     stdev = std(ymat,0, 1);
     n = size(ymat,1);
     sem = stdev / sqrt(n);
+    if isempty(x)
+        x = 1:length(avg);
+    end
     h = shadedErrorBar(x, avg,sem, 'lineProps',color);
     h.mainLine.DisplayName = name;
 end
