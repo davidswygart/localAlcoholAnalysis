a31ce5d29e0feab89fb7aaaaec6eb6ab002aa8b2
davidswygart <davidswygart@gmail.com>
2024-03-07 14:35:54 -0500

    pad bins to prevent edge effects

--- binAroundValve.m
+++ binAroundValve.m
@@ -8,12 +8,21 @@ for i=1:size(c,1)
 
     for v=1:ndrops
         spk = c.spikeTimes{i} - tVals(v);
-        count = histcounts(spk,binEdges);
         if any(contains(varargin,'smooth'))
-            meanISI = mean(diff(spk));
             binWidth = binEdges(2)-binEdges(1);
-            count = imgaussfilt(count, meanISI/binWidth/4);
+            npads = 5; %number of padding bins to put on each end, prevents end effects caused by smoothing
+            padStart = binEdges(1)-binWidth*npads:binWidth:binEdges(1)-binWidth;
+            padEnd =  binEdges(end)+binWidth:binWidth:binEdges(end)+binWidth*npads;
+            newBins = [padStart, binEdges, padEnd];
+
+            count = histcounts(spk,newBins);
+            meanISI = mean(diff(spk));        
+            count = imgaussfilt(count, meanISI/binWidth/4,'Padding','circular');
+            count = count(npads+1:end-npads); % throw out padded bins
+        else
+            count = histcounts(spk,binEdges);
         end
+
         spkCounts(i,:,v) = count;
     end
 end
