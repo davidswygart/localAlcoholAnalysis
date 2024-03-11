a83731ca46ae1e63723e70651505dc0c2d8d41aa
David Swygart <davidswygart2015@u.northwestern.edu>
2024-03-11 17:53:51 -0400

    Create figsForLabMeeting.m

---
+++ figsForLabMeeting.m
@@ -0,0 +1,140 @@
+%% filter out bad clusters
+goodClusters = allClusters;
+goodClusters = goodClusters(goodClusters.fr>0.1, :);
+goodClusters = goodClusters(~contains(goodClusters.phyLabel,'noise'), :);
+goodClusters = goodClusters(goodClusters.presenceRatio>0.9, :);
+goodClusters = goodClusters(goodClusters.isiViolations<1, :);
+
+%% Give example heatmaps for specific mice (spike count)
+binWidth=0.1;
+binEdges = -10*60:binWidth:60*37;
+target = 'microInjectionStart';
+cLabel = 'spikes';
+cRange = [0,8];
+
+figure(1); clf
+t = tiledlayout(3,1,'TileSpacing','Compact','Padding','Compact');
+
+nexttile
+exampleControl = goodClusters(contains(goodClusters.matName, '2023-11-16_10-56-52'), :);
+[spkCounts,  bpod]  = binAroundTarget(exampleControl, target, binEdges);
+plotSpikeHeatmapWithBpod(spkCounts, binEdges, bpod, cLabel,cRange)
+set(gca,'xticklabel',{[]})
+title('Control')
+
+nexttile
+exampleDrink = goodClusters(contains(goodClusters.matName, '2023-11-15_12-27-09'), :);
+[spkCounts,  bpod]  = binAroundTarget(exampleDrink, target, binEdges);
+plotSpikeHeatmapWithBpod(spkCounts, binEdges, bpod, cLabel,cRange)
+set(gca,'xticklabel',{[]})
+title('Drink')
+
+nexttile
+exampleInj = goodClusters(contains(goodClusters.matName, '2023-11-16_13-16-55'), :);
+[spkCounts,  bpod]  = binAroundTarget(exampleInj, target, binEdges);
+plotSpikeHeatmapWithBpod(spkCounts, binEdges, bpod, cLabel,cRange)
+xlabel('Time (0.1s bins)')
+title('Inject')
+
+saveas(gcf, 'example_spikes.svg')
+
+%% Give example heatmaps for specific mice (spike count - smoothed)
+binWidth=0.1;
+binEdges = -10*60:binWidth:60*37;
+target = 'microInjectionStart';
+cLabel = 'smoothed spikes';
+cRange = [0,8];
+
+figure(2); clf
+t = tiledlayout(3,1,'TileSpacing','Compact','Padding','Compact');
+
+nexttile
+exampleControl = goodClusters(contains(goodClusters.matName, '2023-11-16_10-56-52'), :);
+[spkCounts,  bpod]  = binAroundTarget(exampleControl, target, binEdges, 'smooth');
+plotSpikeHeatmapWithBpod(spkCounts, binEdges, bpod, cLabel,cRange)
+set(gca,'xticklabel',{[]})
+title('Control')
+
+nexttile
+exampleDrink = goodClusters(contains(goodClusters.matName, '2023-11-15_12-27-09'), :);
+[spkCounts,  bpod]  = binAroundTarget(exampleDrink, target, binEdges, 'smooth');
+plotSpikeHeatmapWithBpod(spkCounts, binEdges, bpod, cLabel,cRange)
+set(gca,'xticklabel',{[]})
+title('Drink')
+
+nexttile
+exampleInj = goodClusters(contains(goodClusters.matName, '2023-11-16_13-16-55'), :);
+[spkCounts,  bpod]  = binAroundTarget(exampleInj, target, binEdges, 'smooth');
+plotSpikeHeatmapWithBpod(spkCounts, binEdges, bpod, cLabel,cRange)
+xlabel('Time (0.1s bins)')
+title('Inject')
+
+saveas(gcf, 'example_spikes_smoothed.svg')
+
+%% Give example heatmaps for specific mice (zscore)
+binWidth=0.1;
+binEdges = -10*60:binWidth:60*37;
+target = 'microInjectionStart';
+cLabel = 'zscore';
+cRange = [-.5,8];
+
+figure(3); clf
+t = tiledlayout(3,1,'TileSpacing','Compact','Padding','Compact');
+
+nexttile
+exampleControl = goodClusters(contains(goodClusters.matName, '2023-11-16_10-56-52'), :);
+[spkCounts,  bpod]  = binAroundTarget(exampleControl, target, binEdges, 'smooth');
+spkCounts = zscore(spkCounts,0,2);
+plotSpikeHeatmapWithBpod(spkCounts, binEdges, bpod, cLabel,cRange)
+set(gca,'xticklabel',{[]})
+title('Control')
+
+nexttile
+exampleDrink = goodClusters(contains(goodClusters.matName, '2023-11-15_12-27-09'), :);
+[spkCounts,  bpod]  = binAroundTarget(exampleDrink, target, binEdges, 'smooth');
+spkCounts = zscore(spkCounts,0,2);
+plotSpikeHeatmapWithBpod(spkCounts, binEdges, bpod, cLabel,cRange)
+set(gca,'xticklabel',{[]})
+title('Drink')
+
+nexttile
+exampleInj = goodClusters(contains(goodClusters.matName, '2023-11-16_13-16-55'), :);
+[spkCounts,  bpod]  = binAroundTarget(exampleInj, target, binEdges, 'smooth');
+spkCounts = zscore(spkCounts,0,2);
+plotSpikeHeatmapWithBpod(spkCounts, binEdges, bpod, cLabel,cRange)
+xlabel('Time (0.1s bins)')
+title('Inject')
+
+saveas(gcf, 'example_zscore.svg')
+
+%% spikes around injection 
+binWidth=10;
+binEdges = -200:binWidth:60*12;
+target = 'microInjectionStart';
+cLabel = 'zscore';
+cRange = [-.5,8];
+
+[spkCounts,  bpod]  = binAroundTarget(goodClusters, target, binEdges, 'smooth');
+spkZ = zscore(spkCounts,0, 2);
+
+isControl = contains(goodClusters.group,'control') | contains(goodClusters.group,'drink');
+control = spkZ(isControl,:);
+isInject = contains(goodClusters.group,'inject');
+inject = spkZ(isInject,:);
+
+%% spikes around injection (heatmap)
+figure(4); clf
+t = tiledlayout(2,1,'TileSpacing','Compact','Padding','Compact');
+
+nexttile
+plotSpikeHeatmapWithBpod(control, binEdges, bpod, cLabel,cRange)
+set(gca,'xticklabel',{[]})
+title('Control')
+
+nexttile
+plotSpikeHeatmapWithBpod(inject, binEdges, bpod, cLabel,cRange)
+title('Inject')
+xlabel('Time (10s bins)')
+
+
+
