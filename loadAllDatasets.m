df041a69182b41151f36e6c357e0d0aea0242d6f
David Swygart <davidswygart2015@u.northwestern.edu>
2024-03-11 17:53:36 -0400

    Update loadAllDatasets.m

--- loadAllDatasets.m
+++ loadAllDatasets.m
@@ -1,5 +1,5 @@
 
-load('C:\Users\david\OneDrive - Indiana University\localAlcohol\ephysData\group.mat')
+load('C:\Users\dis006\OneDrive - Indiana University\localAlcohol\ephysData\group.mat')
 matFiles = string(ls('*.mat'));
 nFiles = size(matFiles,1);
 
@@ -7,14 +7,16 @@ allClusters = table();
 
 for i = 1:nFiles
     d = load(matFiles(i));
+    g = group(contains(group.matName, erase(matFiles(i),'.mat')), :);
+    
+    if isempty(g)
+        continue
+    end
+
+    g = repmat(g, size(d.clusterInfo,1),1);
     clusters = d.clusterInfo;
     clusters = renamevars(clusters,'group','spikeInterfaceLabel');
-
-    clusters.bpod(:) = extractBpodTimes(d.events);
-
-    g = group(contains(group.matName, erase(matFiles(i),'.mat')), :);
-    g = repmat(g, size(d.clusterInfo,1),1);
-    
+    clusters.bpod(:) = extractBpodTimes(d.events);    
     clusters = [clusters, g];
     allClusters =[allClusters;clusters];
 end
