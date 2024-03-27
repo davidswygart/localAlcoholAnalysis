d969cd3d480a401277a63d3b97dbc1f8c3c5a3c0
David Swygart <davidswygart2015@u.northwestern.edu>
2024-03-27 13:27:37 -0400

    add cross correlation of concentration vs spiking

--- FiringVsConcentration.m
+++ FiringVsConcentration.m
@@ -81,13 +81,13 @@ xp = 0:2:160;
 hold on
 shadedErrorBar(xp,yp,delta)
 xlim([0,165])
-%% Correlation of firing to concentration (mountain plot)
- [rho,pval] = corr(conc',spkCounts');
+%% Correlation of firing to concentration (lag = 0)
+ [rho,pval] = corr(conc',spkZ');
 
  identity = logical(eye(length(rho)));
  pval = pval(identity);
  rho = rho(identity);
-
+%% Mountain plot correlation lag 0
 figure(5); clf
 [f,x] = ecdf(rho(isControl));
 f(f>0.5) = 1 - f(f>0.5);
@@ -117,7 +117,7 @@ stack(2,2) = sum(pval(isInject)>.05);
 stack(3,2) = sum((rho(isInject)>0) & (pval(isInject)<.05));
 stack(:,2) = stack(:,2) / sum(stack(:,2));
 
-barh(["Control","Inject"],stack', 'stacked')
+barh(categorical(["Control","Inject"]), stack', 'stacked')
 %ylim([0,sum(control_stack)])
 xlabel('Proportion of clusters')
 
@@ -131,7 +131,101 @@ textX = stack(1,2) + stack(2,2) + stack(3,2)/2;
 text(textX,textY, '+Corr','HorizontalAlignment','center','Color',c(3,:),'FontSize',20)
 
 
-%%
+%% Scatter plot correlation vs distance
+figure(7); clf
+
+subplot(2,1,1)
+x = goodClusters.distFromInj(isControl);
+y = rho(isControl);
+scatter(x,y, 'filled', 'MarkerEdgeAlpha',0, 'MarkerFaceAlpha',.8)
+xlim([600,1500])
+title('Control')
+ylabel('Correlation: Spiking vs concentration')
+
+subplot(2,1,2)
+x = goodClusters.distFromInj(isInject);
+y = rho(isInject);
+scatter(x,y, 'filled', 'MarkerEdgeAlpha',0, 'MarkerFaceAlpha',.8)
+xlim([600,1500])
+title('Inject')
+ylabel('Correlation: spiking vs concentration')
+xlabel('Distance from injection (um)')
+
+%% Cross correlation
+[r,lag] = xcorr(conc(1,:), spkZ(1,:));
+r = nan(size(conc,1), length(r));
+
+for i=1:size(conc,1)
+    r(i,:) = xcorr(conc(i,:), spkZ(i,:), 'normalized');
+end
+
+%% Plot mean cross correlation (all)
+figure(8); clf
+x = lag*binWidth;
+addShadedLine(x, r, 'k','All')
+hold on
+%ylim([0,1])
+plot([0,0],ylim,'k--')
+plot(xlim,[0,0], 'k--')
+xlabel('Lag (s)')
+ylabel('Correlation: spiking vs concentration')
+
+%% Plot mean cross correlation (split by type)
+figure(8); clf
+x = lag*binWidth;
+addShadedLine(x, r(isControl,:), 'b','Control')
+hold on
+addShadedLine(x, r(isInject,:), 'r','Inject')
+%ylim([0,1])
+plot([0,0],ylim,'k--')
+plot(xlim,[0,0], 'k--')
+xlabel('Lag (s)')
+ylabel('Correlation: spiking vs concentration')
+legend("Control", "Inject")
+
+%% Mountain plot of p-Value for specific lag
+lagPoint = 90;
+%lagPoint = -360;
+[~,lagInd] = min(abs(lag-lagPoint/binWidth));
+
+figure(9); clf
+[f,x] = ecdf(r(isControl,lagInd));
+f(f>0.5) = 1 - f(f>0.5);
+plot(x,f,'LineWidth',2,'Color','b')
+hold on
+
+[f,x] = ecdf(r(isInject,lagInd));
+f(f>0.5) = 1 - f(f>0.5);
+plot(x,f,'LineWidth',2,'Color','r')
+
+xlabel('Correlation (firing to ethanol concentration)')
+ylabel('Folded probability')
+
+title(['Spike lag ', num2str(lagPoint), 's'])
+
+plot([0,0],[0,.5], 'k--')
+legend("Control","Inject")
+
+%% Scatter plot correlation vs distance for lag point
+figure(10); clf
+
+subplot(2,1,1)
+x = goodClusters.distFromInj(isControl);
+y = r(isControl, lagInd);
+scatter(x,y, 'filled', 'MarkerEdgeAlpha',0, 'MarkerFaceAlpha',.8)
+xlim([600,1500])
+title('Control')
+ylabel('Correlation: Spiking vs concentration')
+
+subplot(2,1,2)
+x = goodClusters.distFromInj(isInject);
+y = r(isInject, lagInd);
+scatter(x,y, 'filled', 'MarkerEdgeAlpha',0, 'MarkerFaceAlpha',.8)
+xlim([600,1500])
+title('Inject')
+ylabel('Correlation: spiking vs concentration')
+xlabel('Distance from injection (um)')
+%% calc concentration function
 function conc = calcConcentrationForCluster(clusters, diffusion,binEdges)
     t = binEdges(1:end-1) + (binEdges(2)-binEdges(1))/2;
     
